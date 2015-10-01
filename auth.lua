2642cb1ffdc868b2140fe0a5aa99fd7699940749
Flavio Sousa <FSousa@ip-172-16-65-56.eu-west-1.compute.internal>
2015-10-01 14:30:07 +0100

    removing apiKey from query string

--- auth.lua
+++ auth.lua
@@ -6,12 +6,16 @@ local timestamp = ""
 local validateToken = require 'lib/validate-token'
 
 if query_args ~= nil then
- 
+  local apiKeybase64 = query_args["apiKey"]
   local valid = validateToken(query_args["apiKey"])
 
   if valid == true then 
+    query_args["apiKey"] = nil --clean up apiKey
+    local newArgs = query_args --assign new apiKey
+    ngx.req.set_uri_args(newArgs)
     return  
   end
+  
 
 end
 
